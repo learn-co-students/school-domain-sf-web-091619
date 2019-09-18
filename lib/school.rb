5e9d9067a45dc92cf1105b26d5dfff51de23dd44
kat-star <katherinewkwong@gmail.com>
2019-09-17 22:22:21 -0700

    lab complete

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+# require 'pry'
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+
+  def add_student(name, grade)
+    # if @roster[grade]
+    #   @roster[grade] = []
+    # end
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.reduce({}) do |memo, (grade, students)| 
+      memo[grade] = students.sort
+      memo
+    end
+  end
+end
